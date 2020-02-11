4e5b79cf86eaf9de8eb7b2aa112a7e9fd77d3110
Jade Rickerts <jade.rickerts@gmail.com>
2020-02-11 05:20:50 +0200

    remove models from startup page

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BudgetApp.Models;
 using Microsoft.EntityFrameworkCore;
 using BudgetApp.Data;
 
